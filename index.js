932fe566dee3f935555b8811440fe94b49abf263
schwamster <bastian.toepfer@gmail.com>
2018-09-25 17:21:23 +0200

    use serverless credentials reporting implemented enabled feature

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
-const AWS = require('aws-sdk');
 const delay = require('delay');
+const chalk = require('chalk');
 
 class CreateCertificatePlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
-    this.provider = this.serverless.getProvider('aws');
+    this.initialized = false;
 
     this.commands = {
       'create-cert': {
@@ -19,46 +19,111 @@ class CreateCertificatePlugin {
 
     this.hooks = {
       'create-cert:create': this.createCertificate.bind(this),
+      'after:deploy:deploy': this.certificateSummary.bind(this),
+      'after:info:info': this.certificateSummary.bind(this),
     };
   }
 
+  initializeVariables() {
+    if (!this.initialized) {
+      this.enabled = this.evaluateEnabled();
+      if (this.enabled) {
+        const credentials = this.serverless.providers.aws.getCredentials();
+        this.route53 = new this.serverless.providers.aws.sdk.Route53(credentials);
+        this.region = this.serverless.service.custom.customCertificate.region || 'us-east-1';
+        this.domain = this.serverless.service.custom.customCertificate.certificateName;
+        const acmCredentials = Object.assign({}, credentials, { region: this.region });
+        this.acm = new this.serverless.providers.aws.sdk.ACM(acmCredentials);
+      }
+
+      this.initialized = true;
+    }
+  }
+
+
+
+  /**
+   * Determines whether this plug-in should be enabled.
+   *
+   * This method reads the customDomain property "enabled" to see if this plug-in should be enabled.
+   * If the property's value is undefined, a default value of true is assumed (for backwards
+   * compatibility).
+   * If the property's value is provided, this should be boolean, otherwise an exception is thrown.
+   */
+  evaluateEnabled() {
+    const enabled = this.serverless.service.custom.customDomain.enabled;
+    if (enabled === undefined) {
+      return true;
+    }
+    if (typeof enabled === 'boolean') {
+      return enabled;
+    } else if (typeof enabled === 'string' && enabled === 'true') {
+      return true;
+    } else if (typeof enabled === 'string' && enabled === 'false') {
+      return false;
+    }
+    throw new Error(`serverless-certificate-creator: Ambiguous enablement boolean: '${enabled}'`);
+  }
+
+  reportDisabled() {
+    return Promise.resolve()
+      .then(() => this.serverless.cli.log('serverless-certificate-creator: Custom domain is disabled.'));
+  }
+
+  listCertificates() {
+    return this.acm.listCertificates({}).promise();
+  }
+
+  getExistingCertificate() {
+    return this.listCertificates().then(data => {
+
+      let existingCerts = data.CertificateSummaryList.filter(cert => cert.DomainName === this.domain);
+      if (existingCerts.length > 0) {
+        return existingCerts[0];
+      }
+      return undefined;
+    });
+  }
+
+
+
+  /**
+   * Creates a certificate for the given options set in serverless.yml under custom->customCertificate
+   */
   createCertificate() {
-    let region = this.serverless.service.custom.customCertificate.region || 'us-east-1';
-    let domain = this.serverless.service.custom.customCertificate.certificateName;
-    let idempotencyToken = this.serverless.service.custom.customCertificate.idempotencyToken;
 
+    this.initializeVariables();
+    if (!this.enabled) {
+      return this.reportDisabled();
+    }
+    this.serverless.cli.log(`Trying to create certificate for ${this.domain} in ${this.region} ...`);
+    return this.getExistingCertificate().then(existingCert => {
 
-    this.serverless.cli.log(`Trying to create certificate for ${domain} in ${region} ...`);
-
-    let acm = new AWS.ACM({ apiVersion: '2015-12-08', region: region });
-    let params = {}
-    return acm.listCertificates(params).promise().then(data => {
-
-      if(data.CertificateSummaryList.filter(cert => cert.DomainName === domain).length > 0){
-        this.serverless.cli.log(`Certificate for ${domain} in ${region} already exists. Skipping ...`);
+      if (existingCert) {
+        this.serverless.cli.log(`Certificate for ${this.domain} in ${this.region} already exists. Skipping ...`);
         return;
       }
 
       let params = {
-        DomainName: domain,
+        DomainName: this.domain,
         ValidationMethod: 'DNS'
       };
 
+
+      let idempotencyToken = this.serverless.service.custom.customCertificate.idempotencyToken;
       if (idempotencyToken) {
         Object.assign({}, params, { IdempotencyToken: idempotencyToken })
       }
 
-      return acm.requestCertificate(params).promise().then(requestCertificateResponse => {
+      return this.acm.requestCertificate(params).promise().then(requestCertificateResponse => {
         this.serverless.cli.log('requested cert:' + JSON.stringify(requestCertificateResponse));
 
         var params = {
           CertificateArn: requestCertificateResponse.CertificateArn
         };
 
-        return delay(10000).then(() => acm.describeCertificate(params).promise().then(certificate => {
+        return delay(10000).then(() => this.acm.describeCertificate(params).promise().then(certificate => {
           this.serverless.cli.log('got cert info: ' + JSON.stringify(certificate));
-
-          var route53 = new AWS.Route53({ apiVersion: '2013-04-01' });
           var params = {
             ChangeBatch: {
               Changes: [
@@ -80,7 +145,7 @@ class CreateCertificatePlugin {
             },
             HostedZoneId: this.serverless.service.custom.customCertificate.hostedZoneId
           };
-          route53.changeResourceRecordSets(params).promise().then(recordSetResult => {
+          this.route53.changeResourceRecordSets(params).promise().then(recordSetResult => {
             this.serverless.cli.log('dns validation record created - soon the certificate is functional');
             console.log(JSON.stringify(recordSetResult));
           }).catch(error => {
@@ -109,6 +174,25 @@ class CreateCertificatePlugin {
       throw error;
     })
   }
+
+  /**
+   * Prints out a summary of all domain manager related info
+   */
+  certificateSummary() {
+    this.initializeVariables();
+    if (!this.enabled) {
+      return this.reportDisabled();
+    }
+    return this.getExistingCertificate().then(existingCertificate => {
+      this.serverless.cli.consoleLog(chalk.yellow.underline('Serverless Certificate Creator Summary'));
+
+      this.serverless.cli.consoleLog(chalk.yellow('Certificate'));
+      this.serverless.cli.consoleLog(`  ${existingCertificate.CertificateArn} => ${existingCertificate.DomainName}`);
+      return true;
+    });
+  }
 }
+
+
 
 module.exports = CreateCertificatePlugin;
